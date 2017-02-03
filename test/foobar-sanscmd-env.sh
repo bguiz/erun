98701822ff665c1e243804078c8ac084576a179b
bguiz <github@bguiz.com>
2017-02-04 09:50:30 +1100

    =BG= test(env-override): additional two tests =BG= test(env-override): test cases - 2 new ones
    - trim down main test case to test substitution only once
    - test for `env` override with an environment
    - test for `env` override with an environment-subenvironment

---
+++ test/foobar-sanscmd-env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+npm run erun -- foobartest sansCmd-env arg1 arg2 > ./test/output/foobartest-sanscmd-env.out
+ACTUAL=$( cat ./test/output/foobartest-sanscmd-env.out | grep '^foobartest' )
+EXPECTED="foobartest FOO=sansFoo BAR=sansBar FOOBAR=env-sansCmd-sansFoo-sansBar sansCmd env sansCmd env arg1 arg2"
+
+echo "ACTUAL  : ${ACTUAL}"
+echo "EXPECTED: ${EXPECTED}"
+test "${ACTUAL}" == "${EXPECTED}"
